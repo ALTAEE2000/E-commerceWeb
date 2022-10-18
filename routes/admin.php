790f1e10c442535c8c08336ecf1b4a79fdd0ea96
ALTAEE2000 <meram.ali.ziadi@gmail.com>
2022-10-18 20:28:36 +0200

    thsi is

---
+++ routes/admin.php
@@ -0,0 +1,37 @@
+<?php
+
+// use App\Http\Controllers\Admin\LoginController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+
+
+
+
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    Route::middleware('guest:admin')->group(function () {
+        Route::get('/login', [AdminController::class, 'loginAdmin'])->name('login');
+        Route::post('/check', [AdminController::class, 'check'])->name('ckeck');
+        Route::post('/logout', [AdminController::class, 'logout'])->name('logout');
+    });
+
+    Route::middleware('auth:admin')->group(function () {
+        Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+    });
+});
